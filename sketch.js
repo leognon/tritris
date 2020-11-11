167eaa7a6c41edca2862d95bf43712f6d156c61c
Leo Gagnon <leognon@gmail.com>
2020-11-11 14:43:56 -0500

    Removed 4 piece option

--- sketch.js
+++ sketch.js
@@ -1,19 +1,17 @@
 const padding = 25;
 let game;
-let pieces3json;
-let pieces4json;
+let piecesJSON;
 
 let dom = {};
 
 
 function preload() {
-    pieces3json = loadJSON('pieces3.json');
-    pieces4json = loadJSON('pieces4.json');
+    piecesJSON = loadJSON('pieces.json');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    newGame(4, 0);
+    newGame(0);
 
     dom.titleDiv = select('#title');
     dom.titleDiv.style('visibility: visible');
@@ -27,7 +25,7 @@ function setup() {
     dom.level = select('#level');
     dom.newGame = select('#newGame');
     dom.newGame.mousePressed(() => {
-        newGame(dom.triangles.value(), dom.level.value());
+        newGame(dom.level.value());
     });
 
     resizeDOM();
@@ -42,21 +40,14 @@ function draw() {
     game.show(gameX, gameY, gameWidth, gameHeight);
 }
 
-function newGame(pieces, level) {
-    if (pieces == 3) pieces = pieces3json;
-    else if (pieces == 4) pieces = pieces4json;
-    else {
-        console.error('No traingle amount selected.');
-        alert('Please select a correct (3 or 4) amount of triangles.');
-        return;
-    }
+function newGame(level) {
     level = parseInt(level);
     if (level < 0) {
         console.error('Negative level selected');
         alert('Please select a positive level');
         return;
     }
-    game = new Game(pieces, level);
+    game = new Game(piecesJSON, level);
 }
 
 function resizeDOM() {
@@ -80,4 +71,5 @@ function resizeDOM() {
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     resizeDOM();
+    game.redraw = true;
 }
