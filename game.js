7f9e0fb74641182e4cfe914256a8fa5146e6eef8
Leo Gagnon <leognon@gmail.com>
2020-11-11 13:55:38 -0500

    Refactored piece movement to allow for better phasing

--- game.js
+++ game.js
@@ -66,15 +66,18 @@ class Game {
         this.das = 0;
         this.dasMax = msPerFrame * 16; //It takes 16 frames on an NES to fully charge DAS
         this.dasCharged = msPerFrame * 10; //When charged, DAS reset to 10 frames
+
         this.lastFrame = Date.now();
-        this.keyWasPresssed = false;
-        this.rotateLeftWasPressed = false;
-        this.rotateRightWasPressed = false;
 
         this.entryDelay = msPerFrame * 14; //There is a 10 frame entry delay (the time btwn the last piece locking in, and the next spawning)
         this.spawnNextPiece = 0;
 
         this.redraw = true;
+
+        this.leftWasPressed = false;
+        this.rightWasPressed = false;
+        this.zWasPressed = false;
+        this.xWasPressed = false;
     }
 
     update() {
@@ -90,90 +93,100 @@ class Game {
             this.lastMoveDown = now;
             this.redraw = true;
         }
+        if (this.currentPiece !== null) {
+            //If either left is pressed or right is pressed and down isn't
+            const oneKeyPressed =
+                keyIsDown(LEFT_ARROW) != keyIsDown(RIGHT_ARROW) &&
+                !keyIsDown(DOWN_ARROW);
+            let move = false;
+            if (oneKeyPressed) {
+                this.das += deltaTime;
+                if (
+                    (keyIsDown(LEFT_ARROW) && !this.leftWasPressed) ||
+                    (keyIsDown(RIGHT_ARROW) && !this.rightWasPressed)
+                ) {
+                    //If it was tapped, move and reset das
+                    move = true;
+                    this.das = 0;
+                } else if (this.das >= this.dasMax) {
+                    move = true; //Key is being held, keep moving
+                    this.das = this.dasCharged;
+                }
+            }
+            let horzDirection = 0;
+            if (move) {
+                if (keyIsDown(LEFT_ARROW)) horzDirection = -1;
+                if (keyIsDown(RIGHT_ARROW)) horzDirection = 1;
+            }
 
-        //Move the current piece down
-        let pieceSpeed = this.pieceSpeed;
-        if (keyIsDown(DOWN_ARROW)) {
-            pieceSpeed = min(pieceSpeed * 0.5, this.minDownPieceSpeed);
-        }
-        if (
-            this.currentPiece !== null &&
-            Date.now() >= this.lastMoveDown + pieceSpeed
-        ) {
-            this.currentPiece.move(0, 1); //Move the current piece down
-            let validMove = this.isValid(this.currentPiece);
-            if (!validMove) {
-                this.currentPiece.move(0, -1); //Move the piece up, place on board
-                this.grid.addPiece(this.currentPiece);
-                const row = this.currentPiece.getBottomRow();
-                this.spawnNextPiece = now + this.calcEntryDelay(row);
-                this.currentPiece = null; //There is an entry delay for the next piece
-            }
-            this.lastMoveDown = now;
-            this.redraw = true;
-        }
+            const zPressed = keyIsDown(90) && !this.zWasPressed;
+            const xPressed = keyIsDown(88) && !this.xWasPressed;
+            const rotation = (zPressed ? -1 : 0) + (xPressed ? 1 : 0);
 
-        //Move left and right
-        //If both or neither are pressed, don't move
-        let move = false;
-        const oneKeyPressed =
-            keyIsDown(LEFT_ARROW) != keyIsDown(RIGHT_ARROW) &&
-            !keyIsDown(DOWN_ARROW);
-        if (oneKeyPressed && this.currentPiece !== null) {
-            this.das += deltaTime;
-            if (!this.keyWasPressed) {
-                //If it was tapped, move and reset das
-                move = true;
-                this.das = 0;
-            } else if (this.das >= this.dasMax) {
-                //Key is being held, keep moving
-                move = true;
-                this.das = this.dasCharged;
+            let pieceSpeed = this.pieceSpeed;
+            if (keyIsDown(DOWN_ARROW)) {
+                //Pressing down moves twice as fast, or as fast as the min
+                pieceSpeed = min(pieceSpeed * 0.5, this.minDownPieceSpeed);
             }
-        }
-        if (move && this.currentPiece !== null) {
-            let dir = 0;
-            if (keyIsDown(LEFT_ARROW)) dir = -1;
-            if (keyIsDown(RIGHT_ARROW)) dir = 1;
-            this.currentPiece.move(dir, 0);
-            const isValid = this.isValid(this.currentPiece);
-            if (!isValid) {
-                this.das = this.dasMax;
-                this.currentPiece.move(-dir, 0);
+            const moveDown = Date.now() >= this.lastMoveDown + pieceSpeed;
+            if (horzDirection != 0 || rotation != 0 || moveDown) {
+                this.redraw = true; //A piece has moved, so the game must be redrawn
+                const placePiece = this.movePiece(horzDirection, rotation, moveDown);
+                if (placePiece) {
+                    //Place the piece
+                    this.grid.addPiece(this.currentPiece);
+                    const row = this.currentPiece.getBottomRow();
+                    this.spawnNextPiece = now + this.calcEntryDelay(row);
+                    this.currentPiece = null; //There is an entry delay for the next piece
+                } else {
+                    //If the piece was able to just move down, reset the timer
+                    if (moveDown) this.lastMoveDown = Date.now();
+                }
             }
-            this.redraw = true;
         }
 
-        //Rotation
-        if (
-            this.currentPiece !== null &&
-            keyIsDown(90) &&
-            !this.rotateLeftWasPressed
-        ) {
-            this.currentPiece.rotateLeft();
-            const valid = this.isValid(this.currentPiece);
-            if (!valid) {
-                this.currentPiece.rotateRight();
-            }
-            this.redraw = true;
-        }
-        if (
-            this.currentPiece !== null &&
-            keyIsDown(88) &&
-            !this.rotateRightWasPressed
-        ) {
-            this.currentPiece.rotateRight();
-            const valid = this.isValid(this.currentPiece);
-            if (!valid) {
-                this.currentPiece.rotateLeft();
-            }
-            this.redraw = true;
-        }
-
-        this.keyWasPressed = keyIsDown(LEFT_ARROW) || keyIsDown(RIGHT_ARROW);
-        this.rotateLeftWasPressed = keyIsDown(90); //If Z was pressed
-        this.rotateRightWasPressed = keyIsDown(88); //If X was pressed
+        this.leftWasPressed = keyIsDown(LEFT_ARROW);
+        this.rightWasPressed = keyIsDown(RIGHT_ARROW);
+        this.zWasPressed = keyIsDown(90); //If Z was pressed
+        this.xWasPressed = keyIsDown(88); //If X was pressed
         this.lastFrame = Date.now();
+    }
+
+    movePiece(horzDirection, rotation, moveDown) {
+        //Apply all transformations
+        const vertDirection = moveDown ? 1 : 0;
+        this.currentPiece.move(horzDirection, vertDirection);
+        if (rotation == -1) this.currentPiece.rotateLeft();
+        if (rotation == 1) this.currentPiece.rotateRight();
+
+        //Try with all transformations
+        let valid = this.isValid(this.currentPiece);
+        if (valid) {
+            //The piece (possibly) moved horizontally, rotated and moved down
+            return false; //Don't place the piece
+        }
+        //If blocked, undo horz move and maybe wall-charge
+        this.currentPiece.move(-horzDirection, 0);
+        valid = this.isValid(this.currentPiece);
+        if (valid) {
+            //If the piece was block when moving horz, then wall charge
+            this.das = this.dasMax;
+            return false;
+        }
+
+        //If not valid, undo rotation
+        if (rotation == 1) this.currentPiece.rotateLeft();
+        if (rotation == -1) this.currentPiece.rotateRight();
+        valid = this.isValid(this.currentPiece);
+        if (valid) {
+            //The piece was blocked by rotating
+            return false; //Don't place the piece
+        }
+
+        //If it reaches here, the piece was blocked by moving down and should be placed
+        if (moveDown) this.currentPiece.move(0, -1); //Move the piece back up
+        //The extra if statement is incase the pieces are at the top and spawn in other pieces
+        return true; //Place the piece
     }
 
     calcEntryDelay(y) {
